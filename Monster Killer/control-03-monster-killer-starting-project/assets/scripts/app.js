77deef1c9101ee261136c93e981e5cbb480edcf1
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2022-09-03 20:31:50 -0500

    Add files via upload
    Updated through section 4 video 88.

--- Monster Killer/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ Monster Killer/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,12 +1,36 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 5;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
+
+function endRound() {
+    const initialPlayerHealth = currentPlayerHealth;
+    const playerDamge = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamge;
+
+    if (currentPlayerHealth <= 0 && hasBonusLife) {
+        hasBonusLife = false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+        setPlayerHealth(initialPlayerHealth);
+        alert("You would be dead but the bonus life saved you.");
+    }
+
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You lost!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert('You have a draw!');
+    }
+}
 
 function attackMonster(mode) {
     let maxDamage;
@@ -18,15 +42,7 @@ function attackMonster(mode) {
 
     const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
-    const playerDamge = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-    currentPlayerHealth -= playerDamge;
-    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-        alert('You won!');
-    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-        alert('You lost!');
-    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
-        alert('You have a draw!');
-    }
+   endRound();
 }
 
 function attackHandler() {
@@ -37,5 +53,19 @@ function strongAttackHandler() {
     attackMonster('STRONG_ATTACK');
 }
 
+function healPlayerHandler() {
+    let healValue;
+    if (currentPlayerHealth <= chosenMaxLife - HEAL_VALUE) {
+     alert("You can't heal to more than your max initial health.");
+     healValue = chosenMaxLife - currentPlayerHealth;
+    } else {
+        healValue = HEAL_VALUE;
+    }  
+    increasePlayerHealth(HEAL_VALUE);
+    currentPlayerHealth += HEAL_VALUE;
+    endRound();
+}
+
 attackBtn.addEventListener('click', attackHandler);
-strongAttackBtn.addEventListener('click')
+strongAttackBtn.addEventListener('click');
+healBtn.addEventListener('click', healPlayerHandler);
