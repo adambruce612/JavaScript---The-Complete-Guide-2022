5a9c97e84d58ed44f345c3574570ba2cee630847
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-01-20 09:11:43 -0600

    Add files via upload

---
+++ JavaScript---The-Complete-Guide-2022-main/Assignment 1/basics-assignment-1-start/assignment.js
@@ -0,0 +1,26 @@
+//My solution:
+// let userInput = 12;
+// let result;
+
+// result = userInput += 18;
+
+// result = result - 5;
+// result = result * 10;
+// result = result / 2;
+
+// //No, the value did not change
+// alert(result);
+// alert(userInput);
+
+//Course Solution
+let userInput = 20; //could be a const instead
+let result;
+
+result = 18 + userInput;
+
+result = result - 10;
+result = result * 2;
+result = result / 4;
+
+alert(result);
+alert(userInput);
---
+++ JavaScript---The-Complete-Guide-2022-main/Block Scope-Video 113/app.js
@@ -0,0 +1,24 @@
+// var name = 'Max';
+
+// if (name === 'Max') {
+//     let hobbies = ['Sports', 'Cooking'];
+//     console.log(hobbies);
+
+// }
+
+// function greet() {
+//     var age = 30;
+//     var name = 'Manuel';
+//     console.log(name, age, hobbies);
+// }
+
+// console.log(name, hobbies);
+
+// greet();
+'use strict';
+
+const userName = 'Max';
+var undefined = 5;
+
+console.log(userName);
+
