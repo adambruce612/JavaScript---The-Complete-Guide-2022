3464b54a620252f649da4fdc081113698fce5aa3
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-02-14 09:12:43 -0600

    Add files via upload
    Section 7

---
+++ Section 7/Document and Window Object/app.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('input');
+console.dir(input);
+
+const h1 = document.getElementById('main-title');
+
+h1.textContent = 'Some new title';
+h1.style.color = 'white';
+h1.style.backgroundColor = 'black';
+
+const li = document.querySelector('li:last-of-type');
+li.textContent = li.textContent + ' (Changed!)';
+
+const body = document.body;
+
+
+//Selecting elements this way will live update if new elements are added. 
+const listItemElements = document.getElementsByTagName('li');
+//Will select all elements with li tag - does not live update
+//const listItemElements = document.querySelectorAll('li');
+
+for (const listItemEl of listItemElements) {
+    console.dir(listItemEl);
+}
+
+
