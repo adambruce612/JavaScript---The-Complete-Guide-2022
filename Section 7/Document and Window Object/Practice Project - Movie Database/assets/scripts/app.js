4ecedf0eb075703e00d6a0a58578f7abe650dd9f
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-08-09 18:52:21 -0500

    Add files via upload

--- Section 7/Document and Window Object/Practice Project - Movie Database/assets/scripts/app.js
+++ Section 7/Document and Window Object/Practice Project - Movie Database/assets/scripts/app.js
@@ -3,9 +3,34 @@ const addMovieModal = document.getElementById('add-modal'); //Preferred method
 // const addMovieModal = document.body.children[1]; //One more way to select the same element.
 const startAddMovieButton = document.querySelector('header button');
 //const startAddMovieButton = document.querySelector('header').lastElementChild; Another way to select - but if things get changed in the header it will select the wrong thing
+const backdrop = document.getElementById('backdrop');
+// const backdrop = document.body.firstElementChild; Another way to select
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+
+
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible');
+};
 
 const toggleMovieModal = () => { // function() {}
     addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
+};
+
+const cancelAddMovieHandler = () => {
+    toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+    
+};
+
+const backdropClickHandler = () => {
+    toggleMovieModal();
 }
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener('click',)
